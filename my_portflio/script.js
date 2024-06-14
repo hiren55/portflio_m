b817a5eb421ed22304ddb7437e68c6bc3f79a58e
hiren55 <144219086+hiren55@users.noreply.github.com>
2024-06-15 00:03:03 +0530

    Add files via upload

---
+++ my_portflio/script.js
@@ -0,0 +1,31 @@
+let sections = document.querySelector('section');
+let naLinks = document.querySelector('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offset - 150;
+        let height = sec.offseetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >=offset && top < offset + height) {
+            navLinks.forEach(links =>{
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+    
+};
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
